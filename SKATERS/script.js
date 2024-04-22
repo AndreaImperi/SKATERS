df416f93c079a413d930bc325f3f4b9e4ef46f73
Andrea <imperiandrea68@gmail.com>
2024-04-22 18:03:45 +0200

    Sceitte su carosello + pattern + fix vari

--- SKATERS/script.js
+++ SKATERS/script.js
@@ -30,4 +30,30 @@ document.getElementById("Bshop").addEventListener("click", function() {
 document.getElementById("sfondo").addEventListener("mouseenter", function(){
     var bottone = document.getElementById("Bshop");
     
+    document.querySelectorAll('.btn-check').forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            var selectedMap = this.getAttribute('data-src');
+            document.getElementById('map-iframe').setAttribute('src', selectedMap);
+        });
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const map1 = document.getElementById('skateshop');
+        const map2 = document.getElementById('skatepark');
+        const btnradio1 = document.getElementById('btnradio1');
+        const btnradio2 = document.getElementById('btnradio2');
+    
+        btnradio1.addEventListener('click', function() {
+            map1.style.zIndex = '1';
+            map2.style.zIndex = '0';
+        });
+    
+        btnradio2.addEventListener('click', function() {
+            map1.style.zIndex = '0';
+            map2.style.zIndex = '1';
+        });
+    });
+
+
+
 });
